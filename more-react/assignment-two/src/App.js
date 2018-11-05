84802eff7e21cce969d220f5819740d62bfb0f1c
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-11-04 17:16:16 -0800

    finalized assignment

--- more-react/assignment-two/src/App.js
+++ more-react/assignment-two/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Validation from './ValidationComponent/Validation'
+import Char from './Char/Char'
 
 class App extends Component {
+  state = {
+    userInput: ''
+  }
+  
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value});
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+
   render() {
+
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char 
+        character={ch} 
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />
+    });
+
     return (
       <div className="App">
-        
+        <input
+          text="text"
+          value={this.state.userInput}
+          onChange={this.inputChangedHandler} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
+        {charList}
       </div>
     );
   }
