89ee71df318c2ce7f20d8a5258436b8e26b7df50
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-11-12 22:03:40 -0800

    second bind

--- extra-react/src/App.js
+++ extra-react/src/App.js
@@ -28,14 +28,14 @@ class App extends Component {  // class-based component
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, )}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, "Maximilian")}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
